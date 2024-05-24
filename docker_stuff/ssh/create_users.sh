d12a9a3c3a6f114ad359ce60f90fd087b07b9241
Jave01 <62953863+Jave01@users.noreply.github.com>
2024-05-24 15:25:37 +0200

    backup, add readme

--- docker_stuff/ssh/create_users.sh
+++ docker_stuff/ssh/create_users.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-users=(Groot Drax Rocket Gamora Starlord EGO)
-passwds=(
-    pneumonoultramicroscopicsilicovolcanoconiosis # Groot
-    I_4m_7h3_gr34t3st_0f_7h3m_4ll # Drax
-    1_4m_7h3_b357_p1l07 # Rocket
-    1_4m_4_n0n-d4nc3r_0k? # Gamora
-    Y0u_m1gh7_kn0w_m3_45_Starlord # Star-Lord
-    7h3_un1v3rs3_n33d5_t0_b3_m3 # EGO
-)
 home_rights=(
     750 # Groot
     750 # Drax
@@ -18,7 +9,17 @@ home_rights=(
     750 # EGO
 )
 
-# create users only if they don't exist in for-loop
+users=(Groot Drax Rocket Gamora Starlord EGO)
+passwds=(
+    pneumonoultramicroscopicsilicovolcanoconiosis # Groot
+    I_4m_7h3_gr34t3st_0f_7h3m_4ll # Drax
+    1_4m_7h3_b357_p1l07 # Rocket
+    1_4m_4_n0n-d4nc3r_0k? # Gamora
+    Y0u_m1gh7_kn0w_m3_45_Starlord # Star-Lord
+    7h3_un1v3rs3_n33d5_t0_b3_m3 # EGO
+)
+
+# create users only if they don't exist
 for i in "${!users[@]}"; do
     if id "${users[$i]}" &>/dev/null; then
         echo "User ${users[$i]} already exists"
@@ -42,15 +43,18 @@ echo "Ownership changed for EGO and /home"
 
 usermod -aG sudo EGO
 
-groupadd couple
-usermdo -aG couple Starlord
-usermod -aG couple Gamora
-mkdir -p /etc/system/couple
-chown Starlord:couple /etc/system/couple
-chmod 770 /etc/system/couple
-echo "#!/bin/bash\necho You're a dancer" > /etc/system/couple/random.sh
-chmod 4770 /etc/system/couple/random.sh
-chown Starlord:couple /etc/system/couple/random.sh
+# yes I know it's ugly. Idc
+usermod -aG Rocket Gamora
+
+# groupadd couple
+# usermdo -aG couple Starlord
+# usermod -aG couple Gamora
+# mkdir -p /etc/system/couple
+# chown Starlord:couple /etc/system/couple
+# chmod 770 /etc/system/couple
+# echo "#!/bin/bash\necho You're a dancer" > /etc/system/couple/random.sh
+# chmod 4770 /etc/system/couple/random.sh
+# chown Starlord:couple /etc/system/couple/random.sh
 
 mkdir -p /var/run/docker
 chown root:docker /var/run/docker
